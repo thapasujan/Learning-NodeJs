8a294d18867b96a83e4c4e12fadc7d924f99a4f2
Sujan Thapa <84758577+thapasujan@users.noreply.github.com>
2025-06-12 15:12:47 +0545

    add comment

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // This is NodeJs Learning.
 
-const a = 10
-const b = 20
+const app = require("express")()
 
-console.log(a)
-console.log(a+b)
+app.get("/", (req, res) => {
+    // res.send("Hello I am from home page.")
+    res.json({
+        name: "Sujan Thapa",
+        age: 21
+    })
+})
 
-console.log("Welcome to the Node JS")
+app.get("/contact", (req, res) =>{
+    res.send("I am from contact page.")
+} )
+
+app.get("/about", (req, res) => {
+    console.log("I am from about page.")
+    res.send("I am from about page.")
+})
+
+app.listen(3000, (req, res)=>{
+    console.log("Server is running on port 3000")
+})
+
